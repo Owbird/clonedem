22d89f20c1271afa1f5adaf4f0b0fc1f815209ab
owbird <40531477+Owbird@users.noreply.github.com>
2020-03-10 02:01:14 +0000

    Added clondedem.py

---
+++ clonedem.py
@@ -0,0 +1,62 @@
+import requests
+import json
+import os
+
+profile = input("[?] Enter profile: ")
+
+print("\n[*] Gathering data...\n")
+
+url = f"https://api.github.com/users/{profile}/repos"
+
+res = requests.get(url)
+
+data = json.loads(res.text)
+
+db = {}
+
+if ('message' in data):
+
+    print(f"[!] Github profile {profile} does not exist")
+
+    exit()
+
+else:
+
+    print(f"[+] Found {len(data)} repos\n")
+
+for index, info in enumerate(data, 1):
+    
+    print(f"[+] {index}) Repository name: {info['name']}\n[+] Description: {info['description']}")
+    
+    print('-' * 15)
+
+    db.setdefault(index, [info['name'], info['clone_url']])
+
+download = int(input('[?] Enter repo number to download (0 to download all): '))
+
+try:
+
+    if download == 0:
+
+        print(f"\n[*] Downloading all {len(data)} repos")
+
+    for url in data:
+
+        print(f"[*] Downloading {url['name']}\n")
+
+        os.system(f"git clone {url['clone_url']}")
+
+        print(f"[+] Succefully downloaded {url['name']}\n")
+
+    else:
+
+        print(f"[*] Downloading {db[download][0]}")
+
+        os.system(f"git clone {db[download][1]}")
+
+        print(f"[+] Succefully downloaded {db[download][0]}")
+
+except:
+
+    print("[!] Download failed")
+
